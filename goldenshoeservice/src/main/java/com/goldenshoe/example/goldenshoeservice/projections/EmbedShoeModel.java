1bdc09f0101ef280b4ee600994541021d15fb736
Luca2471 <57104182+Luca2471@users.noreply.github.com>
2021-09-27 18:56:58 +0100

    Delete goldenshoeservice directory

--- goldenshoeservice/src/main/java/com/goldenshoe/example/goldenshoeservice/projections/EmbedShoeModel.java
+++
@@ -1,8 +0,0 @@
-package com.goldenshoe.example.goldenshoeservice.projections;
-
-import com.goldenshoe.example.goldenshoeservice.models.ShoeModel;
-import org.springframework.data.rest.core.config.Projection;
-
-@Projection(name = "embedShoeModel", types = ShoeModel.class)
-public interface EmbedShoeModel{
-}
--- goldenshoeservice/src/main/java/com/goldenshoe/example/goldenshoeservice/repositories/shoeModelRepository/ShoeModelRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.goldenshoe.example.goldenshoeservice.repositories.shoeModelRepository;
-
-import com.goldenshoe.example.goldenshoeservice.models.ShoeModel;
-
-import java.util.List;
-
-public interface ShoeModelRepositoryCustom {
-    List<ShoeModel> findAllShoesByGenderAndCategory(String gender, String category);
-}
